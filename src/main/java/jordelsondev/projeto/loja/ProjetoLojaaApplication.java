0f9c36f38c880791accbd1d3a504ec7b10034bfa
jordelson <jordelsonrv@gmail.com>
2024-03-17 11:21:11 -0300

    commit inicial projeto loja

---
+++ src/main/java/jordelsondev/projeto/loja/ProjetoLojaaApplication.java
@@ -0,0 +1,13 @@
+package jordelsondev.projeto.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLojaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLojaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jordelsondev/projeto/loja/ProjetoLojaaApplicationTests.java
@@ -0,0 +1,13 @@
+package jordelsondev.projeto.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLojaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
