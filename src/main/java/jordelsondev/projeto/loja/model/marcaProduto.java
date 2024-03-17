c89ccf2055bf71850341e7dc2e594777d6c12fff
jordelson <jordelsonrv@gmail.com>
2024-03-17 19:47:00 -0300

    Criado classe de Marcar Produto

---
+++ src/main/java/jordelsondev/projeto/loja/model/marcaProduto.java
@@ -0,0 +1,41 @@
+package jordelsondev.projeto.loja.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "sequenc_marca_produto", sequenceName = "sequenc_marca_produto", allocationSize = 1, initialValue = 1)
+public class marcaProduto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "sequenc_marca_produto")
+	private long id;
+
+	private String nomeDesc;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeDesc() {
+		return nomeDesc;
+	}
+
+	public void setNomeDesc(String nomeDesc) {
+		this.nomeDesc = nomeDesc;
+	}
+
+}
