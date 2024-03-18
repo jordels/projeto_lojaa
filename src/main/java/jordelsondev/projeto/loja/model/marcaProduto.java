98b59276cdcda210cd91667ed59797ce4930e87f
jordelson <jordelsonrv@gmail.com>
2024-03-17 22:20:18 -0300

    commit classe categoria produto

--- src/main/java/jordelsondev/projeto/loja/model/marcaProduto.java
+++ src/main/java/jordelsondev/projeto/loja/model/marcaProduto.java
@@ -10,14 +10,17 @@ import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "marca_produto")
+@Table(name = "marca_produto") // AQUI É CRIADA A TABELA COM SEU NOME
+
+// USA ESTÁ ANOTAÇÃO @SequenceGenerator PARA O PRODUTO SER CADASTRADO EM SEQUENCIA NO BANCO DE UM EM UM...
 @SequenceGenerator(name = "sequenc_marca_produto", sequenceName = "sequenc_marca_produto", allocationSize = 1, initialValue = 1)
+
 public class marcaProduto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "sequenc_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenc_marca_produto")
 	private long id;
 
 	private String nomeDesc;
